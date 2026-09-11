cd71e22fcc6b9d9e28c6629ee20b5269cb1b2b16
Julian Abraham <julian.abraham@xaviers.edu.in>
2026-09-11 08:28:58 +0530

    keygen: use upstream crates, drop ZNS forks, use orchard builder directly

--- src/rpc.rs
+++ src/rpc.rs
@@ -3,7 +3,10 @@
 use serde::{Deserialize, Serialize};
 use zcash_protocol::consensus::BlockHeight;
 
+#[cfg(not(feature = "testnet"))]
 const RPC_URL: &str = "http://127.0.0.1:8232";
+#[cfg(feature = "testnet")]
+const RPC_URL: &str = "http://127.0.0.1:9998";
 
 /// Stateless JSON-RPC transport.
 pub struct Rpc;
